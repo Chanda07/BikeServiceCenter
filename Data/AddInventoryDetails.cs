601dc74a53ebaa6ea0aeb77e61473682b62f79b2
chanda07 <chandajaiswal020@gmail.com>
2023-01-23 20:35:22 +0545

    Initial Commit

---
+++ Data/AddInventoryDetails.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeServiceCenter.Data
+{
+    public class AddInventoryDetails
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public string itemname { get; set; }
+
+        public int quantity { get; set; }
+
+        public string addedby { get; set; }
+
+        public DateTime dateitemadded { get; set; } 
+
+
+    }
+}
---
+++ Data/TakeOutDetails.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeServiceCenter.Data
+{
+    public class TakeOutDetails
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public int id { get; set; }
+        public string itemname { get; set; }
+        public int quantity { get; set; }
+        public string staffname { get; set; }
+        public string approvedby { get; set; }
+        public DateTime datetaken { get; set; }
+        public DateTime dateapproved { get; set; }
+
+        public bool IsDone { get; set; }
+
+        public int totalquantity { get; set; }
+    }
+}
